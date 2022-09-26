d1188fcdb4155fb9b8c853cb42f1347dd04dad7e
GA237 <GA237@WVD-STU-2>
2022-09-26 14:06:35 +0100

    Add project files.

---
+++ Assessment1.1/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace Assessment1._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.generate_meals_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // generate_meals_btn
+            // 
+            this.generate_meals_btn.Font = new System.Drawing.Font("Bahnschrift", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.generate_meals_btn.Location = new System.Drawing.Point(296, 204);
+            this.generate_meals_btn.Name = "generate_meals_btn";
+            this.generate_meals_btn.Size = new System.Drawing.Size(178, 47);
+            this.generate_meals_btn.TabIndex = 0;
+            this.generate_meals_btn.Text = "Plan My Meals!";
+            this.generate_meals_btn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.generate_meals_btn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Meal Allocator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button generate_meals_btn;
+    }
+}
+
